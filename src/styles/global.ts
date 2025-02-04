3d454916c061a4759656d534e64addc872392409
Jocelito Junior <jocelito64@hotmail.com>
2025-02-04 14:41:57 -0300

    feat: style defult ul

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -21,4 +21,10 @@ export const GlobalStyle = createGlobalStyle`
   body, input, text-area, button {
     font: 400 1rem Nunito, sans-serif;
   }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
 `;
